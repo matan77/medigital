d4faf44f56ca159ccfb827dbc2cf2c63d69fd363
matan77 <83426124+matan77@users.noreply.github.com>
2023-06-11 19:00:16 +0300

    finished project functionality

--- controller/actions.js
+++ controller/actions.js
@@ -22,19 +22,17 @@ router.get('/products/:id', async (req, res) => {
     const id = req.params.id;
     Category.findByPk(id)
         .then(category => {
-
+            if (category === null) {
+                res.redirect('/dashboard');
+                return;
+            }
             Product.findAll({ where: { categoryId: id } })
                 .then(products => {
-
-                    Category.findAll()
-                        .then(categories => {
-                            res.render('products', {
-                                pageTitle: 'Edit ' + category.CategoryName,
-                                category: category,
-                                products: products,
-                                categories: categories
-                            })
-                        })
+                    res.render('products', {
+                        pageTitle: 'Edit ' + category.categoryName,
+                        category: category,
+                        products: products
+                    })
                 })
         })
         .catch(error => {
@@ -44,6 +42,30 @@ router.get('/products/:id', async (req, res) => {
         })
 })
 
+
+router.get('/product/:id', async (req, res) => {
+    const id = req.params.id;
+    Product.findByPk(id)
+        .then(product => {
+            Category.findAll()
+                .then(categories => {
+                    if (product == null) {
+                        res.redirect('/dashboard');
+                        return;
+                    }
+                    res.render('product', {
+                        pageTitle: 'Edit ' + product.productName,
+                        product: product,
+                        categories: categories
+                    });
+                })
+        })
+        .catch(error => {
+            res.render('index', {
+                pageTitle: 'Welcome to Admin'
+            })
+        })
+});
 
 
 
--- controller/tasks.js
+++ controller/tasks.js
@@ -4,23 +4,110 @@ import Category from '../models/category.js';
 const router = express.Router();
 
 
-router.post('/add_category', async(req,res) => {
-    const {categoryName,categoryMainImage} = req.body;
+router.post('/add_category', async (req, res) => {
+    const { categoryName, categoryImage } = req.body;
     Category.create({
         categoryName: categoryName,
-        categoryMainImage: categoryMainImage
+        categoryImage: categoryImage
     })
-    .then(result => {
-        console.log(result);
-        return res.redirect('/dashboard');
-    })
-    .catch(error => {
-        console.log(error.message);
-        return res.redirect('/dashboard');
-    })
-})
+        .then(result => {
+            console.log(result);
+            return res.redirect('/dashboard');
+        })
+        .catch(error => {
+            console.log(error.message);
+            return res.redirect('/dashboard');
+        })
+});
+
+router.post('/edit_category/:id', async (req, res) => {
+    const { categoryName, categoryImage } = req.body;
+    const id = req.params.id;
+    Category.update({ categoryName: categoryName, categoryImage: categoryImage },
+        { where: { id: id } })
+        .then(result => {
+            console.log(result);
+            return res.redirect('/products/' + id);
+        })
+        .catch(error => {
+            console.log(error.message);
+            return res.redirect('/products' + id);
+        })
+});
+
+router.post('/delete_category/:id', async (req, res) => {
+    const id = req.params.id;
+    Category.destroy({ where: { id: id } })
+        .then(result => {
+            Product.destroy({ where: { categoryId: id } })
+            console.log(result)
+            return res.redirect('/dashboard')
+        })
+        .catch(error => {
+            console.log(error.message)
+            return res.redirect('/dashboard')
+        })
+});
 
 
+router.post('/add_product/:id', async (req, res) => {
+    const { productName, productPrice, productMainImage, productDescription, isAvailable, unitInStock } = req.body;
+    const categoryId = req.params.id;
+    Product.create({
+        productName: productName,
+        productPrice: productPrice,
+        productMainImage: productMainImage,
+        productDescription: productDescription,
+        isAvailable: isAvailable,
+        unitInStock: unitInStock,
+        categoryId: categoryId
+    })
+        .then(result => {
+            console.log(result);
+            return res.redirect('/products/' + categoryId)
+        })
+        .catch(error => {
+            console.log(error.message);
+            return res.redirect('/products/' + categoryId)
+        })
+});
+
+
+
+router.post('/edit_product/:id', async (req, res) => {
+    const { productName, productPrice, productMainImage, productDescription, isAvailable, unitInStock, categoryId } = req.body;
+    const id = req.params.id;
+    Product.update({
+        productName: productName,
+        productPrice: productPrice,
+        productMainImage: productMainImage,
+        productDescription: productDescription,
+        isAvailable: isAvailable,
+        unitInStock: unitInStock,
+        categoryId: categoryId
+    },
+        { where: { id: id } })
+        .then(result => {
+            console.log(result);
+            return res.redirect('/products/' + categoryId);
+        })
+        .catch(error => {
+            console.log(error.message);
+            return res.redirect('/products/' + categoryId);
+        })
+});
+
+router.post('/delete_product/:id', async (req, res) => {
+    const id = req.params.id;
+    Product.destroy({ where: { id: id } }).then(result => {
+        console.log(result)
+        return res.redirect('/dashboard')
+    })
+        .catch(error => {
+            console.log(error.message)
+            return res.redirect('/dashboard')
+        })
+});
 
 
 export default router;
